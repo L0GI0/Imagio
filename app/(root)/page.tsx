3bcf5b7a1a4608ec6ce935477c402908e85d47a9
L0GI0 <logiocorp@gmail.com>
2024-04-02 15:34:55 +0200

    feat: add layout sidebar and mobile navigation

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { UserButton } from '@clerk/nextjs';
+
+function Home() {
+  return (
+    <div>
+      <p>Home</p>
+
+      <UserButton afterSignOutUrl='/' />
+      </div>
+  )
+}
+
+export default Home
